855f29ba45301fb1fb53784cf212da256a568165
jelenas23 <js20190188@student.fon.bg.ac.rs>
2023-02-06 20:37:06 +0100

    Kreiran model Pesma.php

---
+++ model/Pesma.php
@@ -0,0 +1,44 @@
+<?php
+
+class Pesma
+{
+    public $id;
+    public $naziv;
+    public $izvodjac;
+    public $zanr;
+    public $trajanje;
+    public $godina;
+    public $korisnik_id;
+    public $created_at;
+
+    public function __construct($id,$naziv,$izvodjac,$zanr,$trajanje,$godina,$korisnik_id,$created_at) {
+        $this->id = $id;
+        $this->naziv = $naziv;
+        $this->izvodjac = $izvodjac;
+        $this->zanr = $zanr;
+        $this->trajanje = $trajanje;
+        $this->godina = $godina;
+        $this->korisnik = $korisnik_id;
+        $this->created_at = $created_at;
+    }
+
+    public function dodajPesmu()
+    {
+        $host = 'localhost';
+        $user = 'root';
+        $password = '';
+        $database = 'domaci1';
+        $conn = mysqli_connect($host, $user, $password, $database);
+
+        $query = "INSERT INTO pesma (naziv, izvodjac, zanr, trajanje,
+            godina, korisnik_id)  VALUES ($this->naziv, '$this->izvodjac',
+            '$this->zanr', '$this->trajanje' , '$this->godina'
+            , '$this->korisnik_id')";
+
+        if (mysqli_query($conn, $query)) {
+            header('Location: index.php');
+        } else {
+            echo 'Error: ' . mysqli_error($conn);
+        }
+    }
+}
